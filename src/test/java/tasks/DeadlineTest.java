324789f1161d1f5e18c23c7a9980fef9a9f981bf
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-09-04 01:28:40 +0800

    test

--- src/test/java/tasks/DeadlineTest.java
+++ src/test/java/tasks/DeadlineTest.java
@@ -10,12 +10,12 @@ public class DeadlineTest {
     @Test
     public void toStringTestNoTime() {
         Deadline d = new Deadline("Test", "2020-11-11");
-        assertEquals(d.toString(), "[D][ ] Test (by: 11 Nov 2020)");
+        assertEquals(d.toString(), "[D][ ] Test (by: 11 Nov, 2020)");
     }
 
     @Test
     public void toStringTestTime() {
         Deadline d = new Deadline("Test", "2020-11-11, 1800");
-        assertEquals(d.toString(), "[D][ ] Test (by: 11 Nov 2020, 6:00:00 PM)");
+        assertEquals(d.toString(), "[D][ ] Test (by: 11 Nov, 2020, 6:00:00 PM)");
     }
 }
--- src/test/java/tasks/EventTest.java
+++ src/test/java/tasks/EventTest.java
@@ -10,12 +10,12 @@ public class EventTest {
     @Test
     public void toStringTestNoTime() {
         Event e = new Event("Test", "2020-11-11", "2020-11-12");
-        assertEquals(e.toString(), "[E][ ] Test (at: 11 Nov 2020 to 12 Nov 2020)");
+        assertEquals(e.toString(), "[E][ ] Test (at: 11 Nov, 2020 to 12 Nov, 2020)");
     }
 
     @Test
     public void toStringTestTime() {
         Event d = new Event("Test", "2020-11-11, 1800", "2020-11-12, 1800");
-        assertEquals(d.toString(), "[E][ ] Test (at: 11 Nov 2020, 6:00:00 PM to 12 Nov 2020, 6:00:00 PM)");
+        assertEquals(d.toString(), "[E][ ] Test (at: 11 Nov, 2020, 6:00:00 PM to 12 Nov, 2020, 6:00:00 PM)");
     }
 }
